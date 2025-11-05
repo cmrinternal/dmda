d3c67d2e9605d549a4fe5e86a67ea4a252a109d4
pass <phenixfire00@gmail.com>
2025-11-05 20:59:30 +0530

    Update binomial.R

--- binomial.R
+++ binomial.R
@@ -1,7 +1,7 @@
 #dbinom
-x <- seq(0,50,by=1)
+x <- 0:10
 y <- dbinom(x, size = 10, prob = 0.5)
-plot(x, y, type = "p", lwd = 3, col = "blue", main = "Binomial Distribution (PMF)", xlab = "x", ylab = "P(X = x)")
+plot(x, y, type = "h", lwd = 3, col = "blue", main = "Binomial Distribution (PMF)", xlab = "x", ylab = "P(X = x)")
 
 #pbinom
 x<-pbinom(26,51,0.5)
