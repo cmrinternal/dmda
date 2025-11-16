c6a59e3187546af2452b1fb05a7c98e22efb8eab
pass <phenixfire00@gmail.com>
2025-11-16 16:50:09 +0530

    week10
    This script includes time series analysis of snowfall data, non-linear least squares fitting for a set of x and y values, and a classification model using the C5.0 algorithm on the iris dataset.

---
+++ week10.R
@@ -0,0 +1,33 @@
+# TimeSeries
+snowfall<- c(790,1170.8,860.1,1330.6,630.4,911.5,683.5,996.6,783.2,982,881.8,1021)
+snowfall_timeseries<- ts(snowfall,start= c(2013,1),frequency =12)
+print(snowfall_timeseries)
+model<-lm(snowfall_timeseries~time(snowfall_timeseries))
+plot(snowfall_timeseries,main="snowfall Over",xlab="Time" , ylab="Snowfall", col="blue")
+abline(model,col="red")
+-----------------------------------------------------------------------------------------------------------------------
+# non-linear least square
+xvalues<-c(1.6,2.1,2,2.23,3.71,3.25,3.4,3.86,1.19,2.21)
+yvalues<-c(5.19,7.43,6.94,8.11,18.75,14.88,16.06,19.12,3.21,7.58)
+plot(xvalues,yvalues)
+model<-nls(yvalues~b1*xvalues^2+b2,start=list(b1=1,b2=3))
+new.data<-data.frame(xvalues= seq(min(xvalues),max(xvalues),len=100))
+lines(new.data$xvalues,predict(model,newdata=new.data))
+print(sum(resid(model)^2))
+print(confint(model))
+-----------------------------------------------------------------------------------------------------------------------
+data(iris)
+
+library(C50)
+library(caTools)
+
+set.seed(7)
+
+split <- sample.split(iris$Species, SplitRatio = 0.7)
+training <- subset(iris, split == TRUE)
+
+model<-C5.0(Species~.,data=training)
+summary(model)
+a<-table(testing$Species,pred)
+sum(diag(a))/sum(a)
+plot(model)
