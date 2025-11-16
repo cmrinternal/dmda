0b97c2fdbfc75072703f870420de0d0ac568e7a8
pass <phenixfire00@gmail.com>
2025-11-16 16:50:50 +0530

    week12

--- test.R
+++ test.R
@@ -4,7 +4,7 @@ print(str(Cars93))
 car_data <- table(Cars93$AirBags, Cars93$Type)
 print(car_data)
 print(chisq.test(car_data))
-
+ 
 #t-test
 x <- c(0.593, 0.142, 0.329, 0.691, 0.231, 0.793, 0.519, 0.392, 0.418)
 t.test(x, alternative="greater", mu=0.3)
